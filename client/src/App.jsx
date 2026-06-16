4dc567274cf7ad6435603a63ca06b12decae6d16
Yulichka <kukucapolic@gmail.com>
2026-06-17 01:09:37 +0300

    fix db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://kyiv-spots-app.onrender.com';
 function App() {
   const [spots, setSpots] = useState([]);
   const [logs, setLogs] = useState([]);
